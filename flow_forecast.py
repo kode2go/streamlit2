f6f961861db9c4db655c08d2c5713f381f75c45d
kode2go <brsbin001@myuct.ac.za>
2022-11-25 05:57:32 +0200

    Update flow_forecast.py

--- flow_forecast.py
+++ flow_forecast.py
@@ -74,6 +74,11 @@ ZMW = data['rates']['ZMW']
 MZN = data['rates']['MZN']
 ZAR = data['rates']['ZAR']
 
+col1, col2, col3 = st.columns(3)
+col1.metric("BWP", str(BWP) + " P", "1.2%")
+col2.metric("ZMW", str(ZMW) + " K", "-0.2%")
+col3.metric("MZN", str(MZA) + " M", "0.2%")
+
 #Assumptions
 
 country = "Zambia" #Must 'toggle"
