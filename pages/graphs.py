4b9ddbd2bb7f099d90d1841599a9675ad709eb20
kode2go <brsbin001@myuct.ac.za>
2022-11-25 06:07:10 +0200

    Update graphs.py

--- pages/graphs.py
+++ pages/graphs.py
@@ -268,4 +268,11 @@ fig = px.line(df, title = "Cash Flow Forecast")
 st.markdown(" ## Graphs ")
 st.plotly_chart(fig)
 
+import pandas_profiling
+from streamlit_pandas_profiling import st_profile_report
+pr = df2.profile_report()
+st_profile_report(pr)
+
+
+
 
